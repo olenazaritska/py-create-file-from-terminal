b1df8540e7baf2bb91b26169c553f0cbcd48c31c
Olena Zaritska <zaritskaos@gmail.com>
2024-05-27 21:27:15 +0300

    implement changes

--- app/create_file.py
+++ app/create_file.py
@@ -10,14 +10,17 @@ def main() -> None:
     if file_name:
         full_path = get_full_path(directories, file_name)
         with open(full_path, "a") as file:
+            if os.path.getsize(full_path) != 0:
+                file.write("\n\n")
             current = datetime.datetime.now()
-            file.write(current.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            file.write(current.strftime("%Y-%m-%d %H:%M:%S"))
+            line_num = 1
             while True:
-                line = input("Enter content line: ") + "\n"
+                line = input("Enter content line: ")
                 if line.strip() != "stop":
-                    file.write(line)
+                    file.write("\n" + str(line_num) + " " + line)
+                    line_num += 1
                 else:
-                    file.write("\n")
                     break
 
 
@@ -32,8 +35,7 @@ def get_args() -> tuple:
 def create_directories(directories: list | None) -> None:
     if directories:
         dir_path = os.path.join(*directories)
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
+        os.makedirs(dir_path, exist_ok=True)
 
 
 def get_full_path(
